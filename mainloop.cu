7a178b4724f0237a399079dd1001887518ee29dc
Walt <105594844+jwzj720@users.noreply.github.com>
2023-12-15 09:49:56 -0800

    fixing errors

--- mainloop.cu
+++ mainloop.cu
@@ -456,7 +456,7 @@ int main() {
    // Call the kernel function
     for (int n = 0; n < 1000000; n++){
         for (int m = 0 ; m < REPORT_FREQUENCY; m++){
-            struct Cell *pond, uintptr_t *buffer, int *in, uint64_t *prngState
+            struct Cell *pond, uintptr_t *buffer, int *in, uint64_t *prngState;
             run<<<1, 1>>>(d_pond, d_buffer, d_in, d_prngState);
         }
         doReport(n);
